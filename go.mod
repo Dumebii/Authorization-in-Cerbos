d9916f7d70638f5c774b7f94c84838fd49d5d9d5
Dumebi Okolo <okolodumebi@gmail.com>
2024-03-24 20:28:17 +0100

    update code

--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module GO
 
-go 1.17
+go 1.21
+
+toolchain go1.22.1
 
 require (
 	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.32.0-20240221180331-f05a6f4403ce.1 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/bufbuild/protovalidate-go v0.6.0 // indirect
+	github.com/cerbos/cerbos v0.34.0 // indirect
 	github.com/cerbos/cerbos-sdk-go v0.2.3 // indirect
 	github.com/cerbos/cerbos/api/genpb v0.34.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -33,7 +36,7 @@ require (
 	github.com/stretchr/testify v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
-import ("fmt"
-       
-	   "log"
-	   "github.com/cerbos/cerbos-sdk-go/cerbos"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/cerbos/cerbos-sdk-go/cerbos"
 )
 
 func main() {
-  	// Initialize Cerbos client with adress of the PDP
-	cerbosClient, err := client.NewClient("http://cerbos:3592")
+	// Initialize Cerbos client
+	client, err := cerbos.New("unix:/var/sock/cerbos", cerbos.WithTLSCACert("/path/to/ca.crt"))
 	if err != nil {
-		log.Fatalf("Error initializing Cerbos client: %v", err)
+		log.Fatalf("Failed to create client: %v", err)
 	}
+
+	// Create a new Gorilla router
+	r := mux.NewRouter()
+
+	// Define a Gorilla handler function
+	r.HandleFunc("/resource/{id}", func(w http.ResponseWriter, r *http.Request) {
+		// Extract parameters from the request
+		vars := mux.Vars(r)
+		resourceID := vars["id"]
+
+		// Perform authorization check
+		allowed, err := client.IsAllowed(context.TODO(), cerbos.NewPrincipal("sally").WithRoles("user"), cerbos.NewResource("album:object", resourceID), "view")
+		if err != nil {
+			http.Error(w, "Authorization check failed", http.StatusInternalServerError)
+			return
+		}
+
+		if !allowed {
+			http.Error(w, "Unauthorized", http.StatusForbidden)
+			return
+		}
+
+		// Authorized - Proceed with handling the request
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Authorized to view resource"))
+	})
+
+	// Start the HTTP server with the Gorilla router
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
